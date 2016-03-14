8d17cd4b5c55be28a18928ce4cc0513c975ef34c
heke <heke@soyoung.com>
2016-03-14 15:31:42 +0800

    Initial Commit

---
+++ HKRatio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HKRatio
+//
+//  Created by heke on 14/3/16.
+//  Copyright © 2016年 mhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
